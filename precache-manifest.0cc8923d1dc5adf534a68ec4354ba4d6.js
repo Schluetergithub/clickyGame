374021f2967de91576885f851b17bfffa25ed5c5
Schlueter <SchlueterPro@gmail.com>
2018-10-23 19:22:29 -0700

    Updates

---
+++ precache-manifest.0cc8923d1dc5adf534a68ec4354ba4d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5e1124212e913e930db",
+    "url": "/clickyGame/static/js/runtime~main.f5e11242.js"
+  },
+  {
+    "revision": "f22789b029daf1f1113c",
+    "url": "/clickyGame/static/js/main.f22789b0.chunk.js"
+  },
+  {
+    "revision": "6fea6cd249d4f943684f",
+    "url": "/clickyGame/static/js/1.6fea6cd2.chunk.js"
+  },
+  {
+    "revision": "f22789b029daf1f1113c",
+    "url": "/clickyGame/static/css/main.eb645847.chunk.css"
+  },
+  {
+    "revision": "6c057133113d1c5cae8755471015dc43",
+    "url": "/clickyGame/index.html"
+  }
+];
